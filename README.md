2cfb3530c31a8240217a6c53ed7c612e2beebfa6
DeTK <sda2225@gmail.com>
2024-03-04 20:48:54 +0900

    와일드카드문제 수정...

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ComfyUI Node Switcher
 
-왜 아무도 안만든건지? 잘 모르겠지만...
+왜 아무도 안만든건지? 잘 모르겠지만... 내가 못 찾은건지...
+
+![image](https://github.com/DeTK/ComfyUI-Switch/assets/45531377/88e00aea-76fa-46e7-89be-73e22c7491d5)
--- Switch.py
+++ Switch.py
@@ -1,8 +1,11 @@
-class Switch:
-	CATEGORY = "Switch"
-	FUNCTION = "run"
-	RETURN_TYPES = ("*",)
- 
+# 와일드 카드를 속이는 용도? 임팩트팩에서 가져옴
+class AnyType(str):
+    def __ne__(self, __value: object) -> bool:
+        return False
+
+any_typ = AnyType("*")
+
+class NodeSwitch:
 	def __init__(self):
 		pass
 
@@ -10,20 +13,23 @@ class Switch:
 	def INPUT_TYPES(self):
 		return {
 			"required": {
-				"Switch": ("BOOLEAN", { "default": True, "label_on": "case1", "label_off": "case2" }),
+				"DeTK": ("BOOLEAN", { "default": True, "label_on": "case1", "label_off": "case2" }),
 			},
 			"optional": {
-				"case1": ("*",),
-    		"case2": ("*",)
+				"case1": (any_typ,),
+    		"case2": (any_typ,)
 			}
 		}
+	CATEGORY = "Switch"
+	FUNCTION = "run"
+	RETURN_TYPES = (any_typ,)
 
-	def run(self, Switch, case1, case2):
-		return ((case1 if Switch else case2),)
+	def run(self, DeTK, case1, case2):
+		return ((case1 if DeTK else case2),)
 
 NODE_CLASS_MAPPINGS = {
-	"Switch": Switch
+	"NodeSwitch": NodeSwitch
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
-	"Switch": "Switch",
+	"NodeSwitch": "NodeSwitch",
 }
